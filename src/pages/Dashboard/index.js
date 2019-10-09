4d0dc5fab284b52661d197f72a6f86671d1dfdff
jhow98 <jhonatan@fho.edu.br>
2019-10-09 17:26:25 -0300

    Spots

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
 import api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard(){
     const [spots, setSpots] = useState([]);
@@ -10,7 +12,7 @@ export default function Dashboard(){
             const response = await api.get('/dashboard', {
                  headers: { user_id }
             });
-setSpots(response.data)
+            setSpots(response.data)
         }
         loadSpots();
     }, [])
@@ -19,13 +21,17 @@ setSpots(response.data)
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}>
-                            <strong>{spot.company}</strong>
-                            <span>{spot.price}</span>
-                        </header>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />                            <strong>{spot.company}</strong>
+                            <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
+
+            <Link to="/new">
+                <button className="btn">
+                    Cadastrar novo Spot
+                </button>
+            </Link>
         </>
     );
 }
